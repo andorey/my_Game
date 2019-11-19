df844009a848c78389f0a9b2c6c980ce00dec905
andoreyrich <andoreyrich@gmail.com>
2019-11-19 22:10:49 +0100

    Updates

---
+++ precache-manifest.cda55d906d55f255e5e7d1e2a2843955.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ebbc23c8677851d6cfa08c83e4955",
+    "url": "/my_Game/index.html"
+  },
+  {
+    "revision": "1ae5d9e939217c8835e7",
+    "url": "/my_Game/static/css/main.19030c12.chunk.css"
+  },
+  {
+    "revision": "e6fce22b0815bab76371",
+    "url": "/my_Game/static/js/2.9cfa1b75.chunk.js"
+  },
+  {
+    "revision": "1ae5d9e939217c8835e7",
+    "url": "/my_Game/static/js/main.75e3e61d.chunk.js"
+  },
+  {
+    "revision": "b3930a0b40ff08e845ce",
+    "url": "/my_Game/static/js/runtime-main.1748e6a1.js"
+  },
+  {
+    "revision": "aaa4e976174037d843a6b1151d2fccc8",
+    "url": "/my_Game/static/media/laugh-6.aaa4e976.wav"
+  },
+  {
+    "revision": "7200b2147f0754ce934e4461af7fcdb9",
+    "url": "/my_Game/static/media/laugh_2.7200b214.wav"
+  },
+  {
+    "revision": "9c77e7fbb22816c21f6bb09caf956d6a",
+    "url": "/my_Game/static/media/smile.9c77e7fb.png"
+  }
+]);
